b5c3691d8c0f43fefffb0d94a459536a29d30721
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-06-05 07:36:45 +0900

    user, bookモデルのアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books
   validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: 'は半角英数字どちらも含む8文字以上で入力してください' }
   with_options presence: true do
     validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
