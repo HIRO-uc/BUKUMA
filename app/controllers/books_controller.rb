9b8dd8887ee2d1c6b54e157c88ef8b39bf9f13d6
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-06-05 11:07:49 +0900

    bookモデルストロングパラメータの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :authors, :published_date, :image_link, :info_link, :isbn).merge(user_id: current_user.id)
+    params.permit(:title, :authors, :published_date, :image_link, :info_link, :isbn, :status_id).merge(user_id: current_user.id)
   end
 end
