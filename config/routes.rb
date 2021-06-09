7d542d1b32c42d21cfa2032cd96df8e54ef75437
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-06-09 19:29:28 +0900

    書籍編集に関するルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'home#top'
   get 'books/search'
-  resources :books, only: [:create]
+  resources :books, only: [:create, :show, :update, :destroy]
   resources :users, only: [:show]
 end
