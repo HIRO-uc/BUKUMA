83a3e35840d482340815c00542f94aa44ffe0298
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-06-09 22:27:40 +0900

    プロフィール編集に関するアクションの設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,21 @@
 class ProfilesController < ApplicationController
+  before_action :current_path_profile, only: [:edit, :update]
+
+  def edit
+  
+  end
+
+  def update
+    @profile.update(profile_params)
+    redirect_to user_path(@profile.user)
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:image, :nickname, :self_introduction, :website)
+  end
+
+  def current_path_profile
+    @profile = Profile.find(params[:id])
+  end
 end
